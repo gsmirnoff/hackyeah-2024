8162eecd7281d0eb0310a156694e436600e2a1aa
Oleksandr Monakhov <omonakhov@atlassian.com>
2024-09-28 14:50:27 +0200

    Comprehend added

--- build.sh
+++ build.sh
@@ -2,8 +2,16 @@ mkdir -p build
 lambda_bucket=$(aws s3 ls | grep lambdas | cut -d " " -f 3)
 timestamp=$(date +%s)
 
+# prepare transcribe lambda package
 cd lambda/transcribe && zip -r ../../build/transcribe.zip . && cd ../../
 
 sed -i "s/TranscribeFnFilename: ".*"$/TranscribeFnFilename: \"transcribe-${timestamp}.zip\"/g" cloudformation.template
 
 aws s3 cp build/transcribe.zip s3://${lambda_bucket}/transcribe-${timestamp}.zip
+
+# prepare comprehend lambda package
+cd lambda/comprehend && zip -r ../../build/comprehend.zip . && cd ../../
+
+sed -i "s/ComprehendFnFilename: ".*"$/ComprehendFnFilename: \"comprehend-${timestamp}.zip\"/g" cloudformation.template
+
+aws s3 cp build/comprehend.zip s3://${lambda_bucket}/comprehend-${timestamp}.zip
---
+++ lambda/comprehend/comprehend.py
@@ -0,0 +1,55 @@
+import boto3
+import json
+
+
+def lambda_handler(event, context):
+    s3_client = boto3.client('s3')
+    comprehend_client = boto3.client('comprehend')
+
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    output_bucket_name = os.environ['OUTPUT_BUCKET_NAME']
+    output_file_name = key + '-comprehended.json'
+
+    # Get the document from S3
+    s3_response = s3_client.get_object(Bucket=bucket, Key=key)
+    document_content = s3_response['Body'].read().decode('utf-8')
+
+    # Parse the JSON document
+    document = json.loads(document_content)
+
+    # Extract the transcribed text
+    transcribed_text = document['results']['transcripts'][0]['transcript']
+
+    # Use AWS Comprehend to detect entities in the transcribed text
+    try:
+        print(f"Comprehend job started: {job_name}")
+
+        comprehend_response = comprehend_client.detect_entities(Text=transcribed_text, LanguageCode='en')
+
+        print(f"Comprehend job finished: {job_name}")
+    except Exception as e:
+        print(f"Error starting comprehend job: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': f"Error starting comprehend job: {str(e)}"
+        }
+
+    # Write the entities detected in the transcribed text to a JSON file
+    with open('/tmp/' + output_file_name, 'w') as outfile:
+        json.dump(comprehend_response, outfile)
+
+    # Upload the JSON file to S3
+    try:
+        s3_client.upload_file('/tmp/' + output_file_name, output_bucket_name, output_file_name)
+    except Exception as e:
+        print(f"Error writing to bucket comprehend response: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': f"Error writing to bucket comprehend response: {str(e)}"
+        }
+
+    return {
+        'statusCode': 200,
+        'body': comprehend_response
+    }
